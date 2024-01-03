46ca0e47c95c91524575688b1ec2d5e083956b5f
jcleme <clemensjosh@protonmail.com>
2024-01-03 02:13:36 -0500

    release

--- main.ts
+++ main.ts
@@ -1,134 +1,298 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Modal,
+	Notice,
+	RequestUrlParam,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TextComponent,
+	Vault,
+	request,
+	htmlToMarkdown,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GetChostSettings {
+	importPostTags: boolean;
+	importPostComments: boolean;
+	tagSpaceReplacer: "-" | "_";
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GetChostSettings = {
+	importPostTags: false,
+	importPostComments: false,
+	tagSpaceReplacer: "-",
+};
+
+interface Chost {
+	postURL: string;
+	postAuthorUsername: string;
+	postTitle: string | undefined;
+	postTags: string | undefined;
+	postContent: string;
+	// postComments: string[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GetChost extends Plugin {
+	settings: GetChostSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "import-chost-from-URL",
+			name: "Import a Chost from URL (creates a new note)",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				new urlInputModal(this.app, this).open();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// TODO
+		this.addSettingTab(new GetChostSettingsTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async fetchChost(postURL: string): Promise<Chost | undefined> {
+		const requestParam: RequestUrlParam = {
+			url: postURL,
+		};
+		let body: string = "";
+		const resp = await request(requestParam)
+			.then((r) => (body = r.toString()))
+			.catch(() => {
+				new Notice(
+					"Failed to get post. Check your internet connection and the URL."
+				);
+				return undefined;
+			});
+		const extract = this.parseChost(body, postURL);
+		return extract;
+	}
+
+	async parseChost(html: string, url: string): Promise<Chost> {
+		const parsedHTML = new DOMParser().parseFromString(html, "text/html");
+		const post: HTMLElement = parsedHTML.querySelector(
+			"article.co-post-box"
+		);
+		/// cleanup title section, detect and format asks
+		// check for post title
+		if (post.querySelector("a > h3")) {
+			let newEle = document.createElement("h3");
+			newEle.appendText(post.querySelector("a > h3")?.innerHTML);
+			post.querySelector("a:has(h3)")?.replaceWith(newEle);
+		}
+		// check for ask
+		if (post.querySelector("div.co-embedded-ask")) {
+			let children = post.querySelector("div.co-embedded-ask")?.children;
+			let newEle = document.createElement("blockquote"); //.append(..post.querySelector("div.co-embedded-ask")?.innerHTML)
+			while (
+				post.querySelector("div.co-embedded-ask")?.childNodes.length
+			) {
+				newEle.appendChild(
+					post.querySelector("div.co-embedded-ask")?.firstChild
+				);
+			}
+			post.querySelector("div.co-embedded-ask")?.replaceWith(newEle);
+		}
+
+		// strip empty <a> tag (login link)
+		post.querySelector('a[href="https://cohost.org/rc/login"]')?.remove();
+
+		// parse tags if
+		let frontmatterTags: string = "";
+		if (this.settings.importPostTags) {
+			const tags = post.querySelector("div.co-tags > div");
+			frontmatterTags = "---\ntags:";
+			tags?.childNodes.forEach((tagLink) => {
+				frontmatterTags = frontmatterTags.concat(
+					"\n  - " +
+						tagLink.textContent
+							?.substring(1)
+							.replace(" ", this.settings.tagSpaceReplacer)
+				);
+			});
+			frontmatterTags = frontmatterTags.concat("\n---\n");
+		} else {
+			frontmatterTags = "";
+		}
+
+		const postMarkdown = htmlToMarkdown(post);
+		const title = parsedHTML.querySelector("h3:not([class])")?.innerHTML;
+		const author =
+			parsedHTML.querySelector("a[rel=author]")?.innerHTML || "not found";
+		if (author === "not found") {
+			this.handleNotFound("author");
+		}
+		// const comments: string[] = [];
+
+		let chost: Chost = {
+			postURL: url,
+			postAuthorUsername: author,
+			postTitle: title,
+			postTags: frontmatterTags,
+			postContent: postMarkdown,
+			// postComments: [],
+		};
+
+		return chost;
+	}
+
+	async writeChostToFile(targetURL: string, vault: Vault) {
+		const chost: Chost | undefined = await this.fetchChost(targetURL);
+		if (!chost) {
+			this.handleNotFound(targetURL);
+			return;
+		}
+
+		if (chost.postTitle == undefined || chost.postTitle == "") {
+			vault
+				.create(
+					chost.postAuthorUsername +
+						" - " +
+						chost.postURL.replace(
+							"https://cohost.org/" +
+								chost.postAuthorUsername +
+								"/post/",
+							""
+						) +
+						".md",
+					chost.postTags + chost.postContent
+				)
+				.catch((_e) => this.handleFileExists());
+		} else {
+			vault
+				.create(
+					chost.postAuthorUsername + " - " + chost.postTitle + ".md",
+					chost.postTags + chost.postContent
+				)
+				.catch((_e) => this.handleFileExists());
+		}
+	}
+
+	handleNotFound(searchTerm: string) {
+		new Notice(`Post ${searchTerm} not found!`);
+	}
+
+	handleFileExists() {
+		new Notice(`Note with that title already exists!`);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class urlInputModal extends Modal {
+	urlToFetch: string;
+	plugin: GetChost;
+
+	constructor(app: App, plugin: GetChost) {
 		super(app);
+		this.plugin = plugin;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	onOpen(): void {
+		const { contentEl } = this;
+		contentEl.createEl("h2", { text: "input cohost.org URL here" });
 
-	onClose() {
-		const {contentEl} = this;
+		const inputs = contentEl.createDiv("inputs");
+		const urlInput = new TextComponent(inputs).onChange((urlToFetch) => {
+			this.urlToFetch = urlToFetch;
+		});
+		urlInput.inputEl.focus();
+		urlInput.inputEl.addEventListener("keydown", (event) => {
+			if (event.key === "Enter") {
+				this.close();
+			}
+		});
+
+		const controls = contentEl.createDiv("controls");
+		const searchButton = controls.createEl("button", {
+			text: "Search",
+			cls: "mod-cta",
+			attr: {
+				autofocus: true,
+			},
+		});
+		searchButton.addEventListener("click", this.close.bind(this));
+
+		const cancelButton = controls.createEl("button", { text: "Cancel" });
+		cancelButton.addEventListener("click", this.close.bind(this));
+	}
+	async onClose() {
+		let { contentEl } = this;
 		contentEl.empty();
+		if (this.urlToFetch) {
+			this.plugin.writeChostToFile(
+				this.urlToFetch,
+				this.plugin.app.vault
+			);
+		}
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class GetChostSettingsTab extends PluginSettingTab {
+	plugin: GetChost;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GetChost) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Import post tags")
+			.setDesc("Import post tags (if any exist) as Obsidian tags.")
+			.addToggle((component) =>
+				component
+					.setValue(this.plugin.settings.importPostTags)
+					.onChange(async (value) => {
+						this.plugin.settings.importPostTags = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Tag Space Replacement Character")
+			.setDesc(
+				"While cohost.org tags support spaces, Obsidian tags do not. Choose your prefered replacement."
+			)
+			.addDropdown((component) => {
+				component
+					.addOption("-", "Dash (-)")
+					.addOption("_", "Underscore (_)")
+					.setValue(this.plugin.settings.tagSpaceReplacer)
+					.onChange(async (value) => {
+						this.plugin.settings.tagSpaceReplacer = value;
+						await this.plugin.saveSettings();
+					});
+			});
+
+		// new Setting(containerEl)
+		// .setName("Import post comments")
+		// .setDesc("Import post comments (if any exist).")
+		// .addToggle((component) =>
+		// component
+		// .setValue(this.plugin.settings.importPostComments)
+		// .onChange(async (value) => {
+		// this.plugin.settings.importPostComments = value;
+		// await this.plugin.saveSettings();
+		// })
+		// );
 	}
 }
